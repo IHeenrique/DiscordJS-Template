165d5de1da6e1ca1a408d6c2f6ff37d0d65e2993
Luiz Herique <127365533+IHeenrique@users.noreply.github.com>
2024-01-26 14:06:38 -0300

    Bot main file

--- index.js
+++ index.js
@@ -28,32 +28,18 @@ const client = new Discord.Client({
     ]
 })
 
-// ENUS - Creating a collection that saves commands and their aliases
-// ES - Creando una colección que guarda comandos y sus alias
-// PTBR - Criando uma coleção que salva comandos e seus aliases
-client.commands = new Discord.Collection() 
-client.aliases = new Discord.Collection() 
+// ENUS - Creating a collection that saves commands 
+// ES - Creando una colección que guarda comandos 
+// PTBR - Criando uma coleção que salva comandos
+client.slashCommands = new Discord.Collection() 
 
 // ENUS - Exporting the client to use in other files
 // ES - Exportar el cliente para usarlo en otros archivos
 // PTBR - Exportando o client para utilizar em outros arquivos
 module.exports = client
 
-// ENUS -  This is an event, here we are identifying when the "Ready" event is triggered, so every time the bot is connected to Discord, this event will execute what you put in, as an example I am setting a status and sending it to the console when turned on
-// ES - Este es un evento, aquí estamos identificando cuando se activa el evento "Listo", por lo que cada vez que el bot se conecte a Discord, este evento ejecutará lo que usted puso, como ejemplo estoy estableciendo un estado y enviándolo al consola cuando está encendida
-// PTBR - Este é um evento, aqui estamos identificando quando o evento de "Ready" é acionado, então toda vez que a bot for ligada no Discord, esse evento irá executar o que você colocar, como exemplo estou colocando para setar um status e enviar para o console quando for ligado
-client.on("ready", async() => {
-    console.log(`${client.user.username} Started successfully!`) // Sending to the console when your bot starts
-    client.user.setPresence({
-        activities: [
-            {
-                name: `Hello!`, // Name that appears in the playing activitie
-                type: Discord.ActivityType.Playing // The type set to "Playing"
-            }
-        ],
-        status: `online` // Status set to Online! You can use: "online", "idle", "dnd", "invisible"
-    })
-})
+require('./src/handlers/slashCommands')(client)
+require('./src/handlers/events')(client)
 
 // ENUS - Here we are logging into the client, so this makes us connect the bot to our project
 // ES - Aquí estamos iniciando sesión en el cliente, por lo que esto nos hace conectar el bot a nuestro proyecto.
